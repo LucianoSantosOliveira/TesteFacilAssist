3fee314733602fc2cf33fc73eb9b3dd9f7d695e2
Luciano Santos <lucianoliv2@hotmail.com>
2022-03-28 13:40:40 -0300

    Add files via upload

---
+++ Cliente.cs
@@ -0,0 +1,24 @@
+﻿namespace TesteFacilAssist
+{
+    public class Cliente
+    {
+        string nome;
+        string cpf;
+        string? tipoCliente;
+        string sexo;
+        string? situacaoCliente;
+
+
+        public Cliente(string nome, string cpf)
+        {
+            this.nome = nome;
+            this.cpf = cpf;
+        }
+
+        public string Nome { get => nome; set => nome = value; }
+        public string Cpf { get => cpf; set => cpf = value; }
+        public string TipoCliente { get => tipoCliente; set => tipoCliente = value; }
+        public string Sexo { get => sexo; set => sexo = value; }
+        public string SituacaoCliente { get => situacaoCliente; set => situacaoCliente = value; }
+    }
+}
