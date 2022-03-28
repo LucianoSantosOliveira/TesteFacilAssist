1572ea6c9fc60b8897c77faa793463bb406b15ae
Luciano Santos <lucianoliv2@hotmail.com>
2022-03-28 13:40:04 -0300

    Add files via upload

---
+++ Pages/Atualizar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TesteFacilAssist.Pages
+{
+    public class AtualizarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/Cadastrar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TesteFacilAssist.Pages.Shared
+{
+    public class CadastrarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
